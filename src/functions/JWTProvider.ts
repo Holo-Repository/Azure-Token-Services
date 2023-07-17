7e0a081efebc68bc9c738ef57dade762ba7dcf18
Mike_V <michelangelo.valsecchi@gmail.com>
2023-07-18 00:29:31 +0100

    JWT Provider Dummy Implementation

---
+++ src/functions/JWTProvider.ts
@@ -0,0 +1,49 @@
+import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import { ScopeType } from "@fluidframework/azure-client";
+import { generateToken } from "@fluidframework/azure-service-utils";
+
+
+// NOTE: retrieve the key from a secure location.
+const key = "myTenantKey";
+
+
+export async function JWTProvider(req: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+    context.log(`JWTProvider called with query params: ${JSON.stringify(req.query)}`);
+
+    // tenantId, documentId, userId and userName are required parameters
+    const tenantId = req.query.get('tenantId') as string;
+    const documentId = req.query.get('documentId') as string | undefined;
+    const userId = req.query.get('userId') as string;
+    const userName = req.query.get('userName') as string;
+    let scopes = JSON.parse(req.query.get('scopes') ?? '[]') as ScopeType[] | undefined;
+    scopes = scopes && scopes.length > 0 ? scopes : undefined;
+
+    if (!tenantId)
+        return { status: 400, body: "No tenantId provided in query params" };
+
+    if (!key)
+        return { status: 404, body: `No key found for the provided tenantId: ${tenantId}` };
+
+    let user = { name: userName, id: userId };
+
+    // Will generate the token and returned by an ITokenProvider implementation to use with the AzureClient.
+    const token = generateToken(
+        tenantId,
+        key,
+        scopes ?? [ScopeType.DocRead, ScopeType.DocWrite, ScopeType.SummaryWrite],
+        documentId,
+        user
+    );
+
+    return {
+        status: 200,
+        body: token
+    };
+
+};
+
+app.http('JWTProvider', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: JWTProvider
+});
